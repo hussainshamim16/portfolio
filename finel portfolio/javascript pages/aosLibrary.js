e458529408bd64fd9302a5f3d45d9ffb00f63a73
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-07-31 12:03:57 +0500

    js files

---
+++ finel portfolio/javascript pages/aosLibrary.js
@@ -0,0 +1,5 @@
+AOS.init({
+    offset: 190, // offset (in px) from the original trigger point
+    delay: 7, // values from 0 to 3000, with step 50ms
+    duration: 400,
+});
--- finel portfolio/javascript pages/script.js
+++ finel portfolio/javascript pages/script.js
@@ -1,24 +1,61 @@
 
 
-let skills = ["VIDEO EDITOR","GRAPHIC DESIGNER","Fornt End Developer"]
+let skills = ["VIDEO EDITOR", "GRAPHIC DESIGNER", "Fornt End Developer"]
 let Skname = document.getElementById('Skname');
 let parent = document.getElementById('parent');
 count = 0;
-setInterval((pera)=>{
+setInterval((pera) => {
     Skname.innerHTML = '';
-    if(count >= skills.length){
+    if (count >= skills.length) {
         // alert("braaa")
-        count=0;
+        count = 0;
         return
-    }else{
+    } else {
         var nikkalo = skills[count];
         // Skname.innerHTML = skills[count];
-        parent.innerHTML = ` <h2>Hi Everyone <span id="hend">👋</span></h2>
+        parent.innerHTML = `<h2>Hi There !</h2>
         <h1>I AM MUHAMMAD <span>HUSSAIN</span></h1>
         <h1>A <span id="Skname" style=" animation-name: fade;
         animation-duration: 5s;">${nikkalo}</span></h1>
         <h2>and Visual Designer <br> with 03 experience in web development</h2>`;
-        console.log(nikkalo)
+        // console.log(nikkalo)
     }
     count++
-},2000)
+}, 2000)
+
+const dark = document.getElementById('dark')
+const light = document.getElementById('light')
+var a = 5;
+const alignment = (e) => {
+
+    const containerClick = document.getElementById("containerClick");
+    if (a > 2) {
+        containerClick.style.justifyContent = "flex-end";
+        light.href = "./css pages/light.css";
+        e.innerHTML = "Dark";
+        a = 0;
+    } else {
+        light.href = "";
+        containerClick.style.justifyContent = "flex-start";
+        e.innerHTML = "Light";
+        a = 6;
+    }
+}
+
+const navIn = (el)=>{
+    const navOne = document.getElementById("navOne");
+    console.log("click");
+
+
+    if (a > 2) {
+        el.style.color = "white";
+        el.style.opacity = 1;
+        navOne.className = "one show";
+        a = 0;
+    } else {
+        el.style.color = "var(--gr3)";
+        el.style.opacity = 0.8;
+        navOne.className = "one hide";
+        a = 6;
+    }
+}
